259176b977550007818d7a1996552c34a7875424
Siv168 <sybousiv11@gmail.com>
2025-02-08 10:54:49 +0700

    on going

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -77,6 +77,7 @@ const Chart = () => {
 
   return (
     <div className="grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8 ">
+      
       <div className="w-full md:col-span-4 ">
         <h2 className="mb-4 text-lg md:text-2xl font-[lustina]">
           Recent Revenue
